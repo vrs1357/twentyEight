c61f9681daaf55eb7ef0ef36801b2a8a8e73821b
Varun Seshadri <vsesh@student.ubc.ca>
2025-12-31 15:30:12 -0500

    added cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
+import cors from 'cors'
+
 const { Server, Origins, SocketIO } = require('boardgame.io/server');
 const { Game } = require('./Game.js');
+
 
 const server = Server({
   games: [Game],
   origins: [Origins.LOCALHOST],
   transport: new SocketIO,
 });
-
+server.app.use(
+  cors({
+    origin: "https://twenty-eight-game.vercel.app",
+    credentials: true
+  })
+)
 server.run(8000);
