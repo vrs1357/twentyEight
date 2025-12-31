191c84a690c345700d67f54839c42b50a3a04b66
Varun Seshadri <vsesh@student.ubc.ca>
2025-12-31 16:29:25 -0500

    some more fixes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import cors from 'cors';
-import { Server, SocketIO } from 'boardgame.io/dist/cjs/server.js';
+import { Server, Origins, SocketIO } from 'boardgame.io/dist/cjs/server.js';
 import { Game } from './game.js';
 
 const FRONTEND_URL = 'https://twenty-eight-game.vercel.app';
 
 const server = Server({
   games: [Game],
+  origins: [Origins.LOCALHOST, FRONTEND_URL],
   transport: new SocketIO({
     cors: {
       origin: FRONTEND_URL,
@@ -15,7 +16,7 @@ const server = Server({
   }),
 });
 
-// Apply CORS middleware for REST endpoints
+// Apply CORS middleware to REST endpoints
 server.app.use(cors({
   origin: FRONTEND_URL,
   methods: ['GET','POST','OPTIONS'],
@@ -23,4 +24,8 @@ server.app.use(cors({
   credentials: true,
 }));
 
-server.run(8000, { address: '0.0.0.0' });
+server.run({
+  port: 8000,
+  address: '0.0.0.0',
+  callback: () => console.log('boardgame.io server running on port 8000')
+});
